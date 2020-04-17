27e4ecbe600cf153920a923f39f8a3309611ad63
Piotr <piter.gazownik@o2.pl>
2020-04-18 00:33:26 +0200

    Small refactor and report done

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,6 @@ from generators.fill_increasing import fill_increasing
 from generators.fill_random import fill_random
 
 from utils.measure_time import measure_time
-REPEATS = 10000
 
 instances = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 
@@ -14,41 +13,40 @@ def handle_linked_increasing(size):
     linked_list = LinkedList()
     array = fill_increasing(size)
     times = measure_time(array, linked_list)
-    print('LL, append, {} size, increasing, {} ms'.format(size, times['append']))
-    print('LL, find, {} size, increasing, {} ms'.format(size, times['find']))
+    print('LL, append, {} size, increasing, {} s'.format(size, round(times['append'] / size, 5)))
+    print('LL, find, {} size, increasing, {} s'.format(size, round(times['find'] / size, 5)))
 
 
 def handle_linked_random(size):
     linked_list = LinkedList()
     array = fill_random(size)
     times = measure_time(array, linked_list)
-    print('LL, append, {} size, random, {} ms'.format(size, times['append']))
-    print('LL, find, {} size, random, {} ms'.format(size, times['find']))
+    print('LL, append, {} size, random, {} s'.format(size, round(times['append'] / size, 5)))
+    print('LL, find, {} size, random, {} s'.format(size, round(times['find'] / size, 5)))
 
 
 def handle_bst_increasing(size):
     bst = BinarySearchTree()
     array = fill_increasing(size)
     times = measure_time(array, bst)
-    print('BST, append, {} size, increasing, {} ms'.format(size, times['append']))
-    print('BST, find, {} size, increasing, {} ms'.format(size, times['find']))
+    print('BST, append, {} size, increasing, {} s'.format(size, round(times['append'] / size, 5)))
+    print('BST, find, {} size, increasing, {} s'.format(size, round(times['find'] / size, 5)))
 
 
 def handle_bst_random(size):
     bst = BinarySearchTree()
     array = fill_random(size)
     times = measure_time(array, bst)
-    print('BST, append, {} size, random, {} ms'.format(size, times['append']))
-    print('BST, find, {} size, random, {} ms'.format(size, times['find']))
+    print('BST, append, {} size, random, {} s'.format(size, round(times['append'] / size, 5)))
+    print('BST, find, {} size, random, {} s'.format(size, round(times['find'] / size, 5)))
 
 
 def start():
     for instance in instances:
         try:
-            size = instance * REPEATS
-            handle_linked_increasing(size)
-            handle_linked_random(size)
-            handle_bst_random(size)
-            handle_bst_increasing(size)
+            handle_linked_increasing(instance)
+            handle_linked_random(instance)
+            handle_bst_increasing(instance)
+            handle_bst_random(instance)
         except RuntimeError as re:
             print('Error in {}'.format(re))
--- utils/measure_time.py
+++ utils/measure_time.py
@@ -1,18 +1,22 @@
 import time
 ONE_SECOND = 1000
 
+REPEATS = 10
+
 
 def measure_time(array, structure):
     start_time_append = time.time()
-    for i in array:
-        structure.append(i)
+    for _ in range(REPEATS):
+        for i in array:
+            structure.append(i)
     end_time_append = time.time()
-    time_append = round((end_time_append - start_time_append) * ONE_SECOND, 3)
+    time_append = round((end_time_append - start_time_append) * ONE_SECOND / REPEATS, 3)
     start_time_find = time.time()
-    for i in array:
-        structure.find(i)
+    for _ in range(REPEATS):
+        for i in array:
+            structure.find(i)
     end_time_find = time.time()
-    time_find = round((end_time_find - start_time_find) * ONE_SECOND, 3)
+    time_find = round((end_time_find - start_time_find) * ONE_SECOND / REPEATS, 3)
     return {
         'append': time_append,
         'find': time_find
